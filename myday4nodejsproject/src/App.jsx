99e2dfbac4df413bf93caf2ef708e2e942e8a6f7
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-10-03 01:05:45 +0530

    myday4

---
+++ myday4nodejsproject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Components/Home';
+
+const App = () => {
+  return (
+    <div>
+      <h1>App comp</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ myday4nodejsproject/src/Components/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+    const[empData,setEmpData]=useState([])
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    const fetchData=async()=>{
+        await axios.get("http://localhost:4000/api/emp")
+        .then(res=>setEmpData(res.data))
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    console.log(empData);
+
+
+    return (
+        <div>
+            {empData.map((item,index)=>{
+                return(
+                    <>
+                    <div key={index}>
+                        <h2>{item.firstName}</h2>
+                        <h2>{item.lastName}</h2>
+                        <h2>{item.email}</h2>
+                        <h2>{item.position}</h2>
+                    </div>
+                    </>
+                )
+            })}
+        </div>
+    );
+};
+
+export default Home;
